16cb9ba3f0510207b0f3809bec29642bb5e073ad
EC2 Default User <ec2-user@ip-172-31-26-40.ap-northeast-1.compute.internal>
2024-03-29 18:01:49 +0000

    Initial Commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,22 @@
 class FavoritesController < ApplicationController
 	def create
 		book = Book.find(params[:book_id])
-		favorite = current_user.favorites.new(book_id: book.id)
-		favorite.save
-		redirect_to request.referer and return
-		# redirect_to book_path(book)
+		current_user.favorites.create(book_id: book.id)
+		# favorite = current_user.favorites.new(book_id: book.id)
+		# favorite.save
+		# "favorite ="のあたりから 一文でまとめて表すなら
+		# current_user.favorites.create(book_id: book.id)
+		# さらに分けるなら
+		# current_user.favo(params[:book_id])
+		# model↓
+		# def favo(book_id)
+		# favorites.create(book_id, book_id)
+		redirect_to request.referer
 	end
 	def destroy
 		book = Book.find(params[:book_id])
 		favorite = current_user.favorites.find_by(book_id: book.id)
 		favorite.destroy
-		redirect_to request.referer and return
+		redirect_to request.referer
 	end
 end
